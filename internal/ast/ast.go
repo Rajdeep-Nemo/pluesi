fd80e17b8a8a90148771d4cc1dea50bf6abca2eb
Rajdeep Nemo <rajdeepnemo@gmail.com>
2026-03-29 15:31:19 +0530

    feat: define Node, Statement, Expression interfaces and Program struct

--- internal/ast/ast.go
+++ internal/ast/ast.go
@@ -1,1 +1,24 @@
 package ast
+
+// Node is the base interface for all nodes in the tree
+type Node interface {
+	TokenString() string
+	String() string
+}
+
+// Interface for statements
+type Statement interface {
+	Node
+	statementNode()
+}
+
+// Interface for expressions
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// Root node
+type Program struct {
+	Statements []Statement
+}
